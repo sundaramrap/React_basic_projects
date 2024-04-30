40bbfae3fd557fc6b84cd404f1809d23b0f42f99
sundaramrap <pragya.learn.28@gmail.com>
2024-04-30 22:38:51 +0530

    'completed_xustom_react

--- customReact/index.js
+++ customReact/index.js
@@ -1,14 +1,25 @@
+// what to make in a customReact
+// 1. a hyper link displaying to visit site and a h1 tag saying this is made to enhance custom customReact
+// 2. for hyper link we need a elemet a tag  and for h1 tag we need h tag
+// 3. we have to make a place where to display  instanceof
+// 4. a method to how to display it
+// 5.a funtion to make the funtioning of the tag displayed
+
+function customRender(reactElement, customContainer) {
+  const domEle = document.createElement(reactElement.type);
+  domEle.innerHTML = reactElement.children;
+  domEle.setAttribute("href", reactElement.props.href);
+  customContainer.appendChild(domEle);
+}
+
 const reactElement = {
   type: "a",
   props: {
-    href: "http://www.google.com",
-    target: "_blank",
+    href: "htttp//www.google.com",
   },
-  children: "Click to visit Haven",
+  children: "Click here",
 };
 
-const reactContainer = document.getElementById("root");
+const customContainer = document.querySelector("#root");
 
-// react container is needed to show the place where data will be displayed.
-// meathod--> which will have what to display,where to display
-//<a href="google.com">gooogle click me</a>
+customRender(reactElement, customContainer);

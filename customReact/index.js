6457ce0e4a0d4375c9d5f0d48c039e95c02d67fa
sundaramrap <pragya.learn.28@gmail.com>
2024-04-30 19:05:38 +0530

    renderupdate for custom render

--- customReact/index.js
+++ customReact/index.js
@@ -1,1 +1,14 @@
-    
+const reactElement = {
+  type: "a",
+  props: {
+    href: "http://www.google.com",
+    target: "_blank",
+  },
+  children: "Click to visit Haven",
+};
+
+const reactContainer = document.getElementById("root");
+
+// react container is needed to show the place where data will be displayed.
+// meathod--> which will have what to display,where to display
+//<a href="google.com">gooogle click me</a>
